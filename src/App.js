aabae5bbaa4b40e7ed0ab43d353cb6152b7e5806
Badol4 <githubbadol@gmail.com>
2022-04-11 14:46:44 +0600

    login and register is done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import HOise from './componets/hois/HOise';
 function App() {
   return (
     <div className="App">
-      <h1 className='bg-red'>hi</h1>
   
 
       <HOise></HOise>
--- src/componets/hois/HOise.js
+++ src/componets/hois/HOise.js
@@ -1,60 +1,100 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../../Firebase.init";
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 const HOise = () => {
-    const [user, setUser] = useState('')
-    const [email, setEmail] = useState('')
-    const [pass , setPass] =  useState('')
-    const [emailError, setEmailError] = useState('')
-    const [passError, setPassError] = useState('')
-    const [hasAccount, setHasAccount] = useState(false)
-    const login = e => {
-        setEmail(e.target.value);
-    }
-    const password = e => {
-        setPass(e.target.value);
-    }
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passError, setPassError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
+  const [error, setError] = useState("");
+  const [error2, setError2] = useState("")
+  const [register, setRegister] = useState(false)
+  const login = (e) => {
+    setEmail(e.target.value);
+  };
+  const password = (e) => {
+    setPass(e.target.value);
+  };
+  const handleCahnge = e => {
+      setRegister(e.target.checked)
+  }
 
-    const handleLogin = () => {
-        signInWithEmailAndPassword(email, pass, auth)
-        .thne()
-    }
-    const submit = e => {
-        createUserWithEmailAndPassword(auth, email, pass)
-        .then(result => {
-           
+  const submit = (e) => {
+    e.preventDefault();
+
+    if(register == true){
+        signInWithEmailAndPassword(auth, email, pass)
+        .then((result) => {
             console.log(result.user);
-        })
-        .catch(error => {
-            console.error(error)
-        })
-        e.preventDefault();
-    }
+           
+          })
+          .catch((error) => {
+              console.log(error);
+          })
 
+    }else{
+        createUserWithEmailAndPassword(auth, email, pass)
+      .then((result) => {
+        console.log(result.user);
+        // setEmail('');
+        // setPass('');
+      })
+      
+      .catch((error) => {
+        console.error(error);
+        setError2(error.message)
+      });
+
+    }
+   
+
+    
+
+      if (email === "" || email == null) {
+        setError("user dont give an email");
+      } else {
+        setError("");
+      }
+  };
 
   return (
     <div>
       <div className="w-50 mx-auto">
+          <h1>{register ? 'login':'register'}</h1>
         <Form onSubmit={submit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control onBlur={login} type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              
-            </Form.Text>
+            <Form.Control
+              onBlur={login}
+              type="email"
+              placeholder="Enter email"
+            />
+            <p>{error}</p>
+            <Form.Text className="text-muted"></Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control onBlur={password} type="password" placeholder="Password" />
+            <Form.Control
+              onBlur={password}
+              type="password"
+              placeholder="Password"
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+              <p> {error2} </p>
+            <Form.Check onChange={handleCahnge} type="checkbox" label="All ready have an account" />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Login
+          {register ? 'login':'register'}
           </Button>
         </Form>
       </div>
